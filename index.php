2146d730552956d0264c27e9b67a4b43ef3ece9d
Igor Fonseca <igor.suporte@outlook.com>
2018-05-20 12:47:29 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -16,8 +16,21 @@ require_once("config.php");
 //echo json_encode($busca);
 
 //Carrega usuário usando o login e a senha
-$usuario = new usuario();
-$usuario->login("Igor Fonseca","123456789");
+//$usuario = new usuario();
+//$usuario->login("Igor Fonseca","123456789");
+//echo $usuario;
+
+//$aluno = new Usuario("aluno","@luno");
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@lun0");
+//$aluno->insert();
+
+//echo $aluno;
+
+$usuario = new Usuario();
+$usuario->loadById(4);
+$usuario->update("Diretor","D1r3t#)");
+
 echo $usuario;
 
  ?>
